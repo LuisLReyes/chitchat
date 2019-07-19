0eb1f4e75441b0deecc09b4cdea16b5c83f1d838
LuisLReyes <43256028+LuisLReyes@users.noreply.github.com>
2019-07-19 14:01:55 -0400

    Testing

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const mongoose = require('mongoose');
 const chatroomRoutes = express.Router();
 const chatlogRoutes = express.Router();
 const userRoutes = express.Router();
-const session = require('express-session')
+const session = require('express-session');
+const path = require('path');
 const PORT = 4000;
 
 let Chatroom = require('./models/chatroom.model');
@@ -34,8 +35,21 @@ app.use(function(req, res, next) {
     next();
   });
   
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
 
+/*production mode
+if(process.env.NODE_ENV === 'production') {  
+    app.use(express.static(path.join(__dirname, 'client/build')));  
+    app.get('*', (req, res) => {    
+        res.sendfile(path.join(__dirname = 'client/build/index.html'));  
+    })
+};
 
+//build
+app.get('*', (req, res) => {  
+    res.sendFile(path.join(__dirname+'/client/public/index.html'));
+});*/
 
 //Chatroom Functions
 //Get all chatrooms
