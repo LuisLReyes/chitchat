72ace813dcabb3ccd287bab85f88efdec6ce6c8d
LuisLReyes <43256028+LuisLReyes@users.noreply.github.com>
2019-07-19 18:05:56 -0400

    Moving Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,65 @@
-import React, { Component } from 'react';import './App.css';import Navbar from './Components/Layout/Navbar';import { BrowserRouter as Router, Route } from 'react-router-dom';import Footer from './Components/Layout/Footer';import Home from './Components/Layout/Home';import Social from './Components/social/Social';
-class App extends Component {  constructor(props) {    super(props);    this.state = {}    this.connecToServer = this.connecToServer.bind(this);  }
-connecToServer() {    fetch('/');  }
-componentDidMount() {    this.connecToServer();  }
-render() {    return (      <Router>      <div className="container">         <Navbar />         <Route exact path="/" component={Home} />         <Route exact path="/social" component={Social} />         <Footer />      </div>      </Router>    );  }}
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import elementLogin from "./components/login.component";
+import elementHome from "./components/home.component";
+import elementAddGroup from "./components/addgroup.component";
+import elementOne from "./components/element-one.component";
+
+
+class App extends Component {
+
+
+
+
+  render() {
+    return (
+      <Router>
+
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="https://google.com" target="_blank">
+              {/*<img src={} width="30" height="30" alt="google.com" />*/}
+            </a>
+            <Link to="/" className="navbar-brand">Skeleton for Group 15</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Elements</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/login" className="nav-link">Login</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+          <div className="row">
+            <div className="col-3">
+              <div className="row">
+                <div className="col">
+                  <p>Your Groups   </p>
+                </div>
+                <div className="col">
+                  <button className="btn-primary btn-sm">
+                    +
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="col-8">
+              <Route path="/" exact component={elementHome} />
+              <Route path="/login" component={elementLogin} />
+              <Route path="/addgroup" component={elementAddGroup} />
+              <Route path="/testing" component={elementOne} />
+            </div>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
 export default App;
---
+++ client/src/components/element-one.component.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default class ElementOne extends Component {
+
+    apiTester(){
+        axios.get('http://localhost:4000/user/5d23a48fc543581b901b4bb5')
+            .then(res => {
+                console.log(res.data);
+            })
+    }
+    loginTester(){
+        axios.post('http://localhost:4000/user/login',{
+            user_name: 'Luis1234',
+            password: 'hashtesting'
+        })
+        .then(res=> {
+            console.log(res.data);
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <p>Welcome to Chit-Chat.</p>
+                    <p>Log in to have access for enhanced features</p>
+                </div>
+                <div>
+                    <button type="submit" id="loginBtn" className="btn btn-lg btn-primary btn-block" value="Submit">Login</button>
+                    <button type="submit" id="loginBtn" className="btn btn-lg btn-primary btn-block" value="Continue Anonymously">Login</button>
+                    <button id="apiTester" onClick={this.apiTester}>Send an API call</button>
+                </div>
+            </div>
+        )
+    }
+}
